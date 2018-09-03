62ee00a25867038ad4bef4cf6f1c5259040f452a
Rajvir Rai <rsr1992@gmail.com>
2018-09-03 17:15:02 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-def introduction_with_language
+def introduction_with_language(name, language)
   name = "Raj"
   language = "Ruby"
   puts "Hi, my name is #{name} and I am learning to program in #{languge}"
